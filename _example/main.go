3a8e41dd658f6cc894cb58a105ce2e61b8ca74a1
num5 <guhao19890412>
2017-04-27 17:40:12 +0800

    fix

--- _example/main.go
+++ _example/main.go
@@ -14,22 +14,22 @@ func main() {
 		panic(err)
 	}
 
-	bot.Handle(`/msg/solo`, func(evt wechat.Event) {
-		data := evt.Data.(wechat.EventMsgData)
+	bot.Handle(`/msg/solo`, func(evt webot.Event) {
+		data := evt.Data.(webot.EventMsgData)
 		fmt.Println(`/msg/solo/` + data.Content)
 	})
 
-	bot.Handle(`/msg/group`, func(evt wechat.Event) {
-		data := evt.Data.(wechat.EventMsgData)
+	bot.Handle(`/msg/group`, func(evt webot.Event) {
+		data := evt.Data.(webot.EventMsgData)
 		fmt.Println(`/msg/group/` + data.Content)
 	})
 
-	bot.Handle(`/contact`, func(evt wechat.Event) {
-		data := evt.Data.(wechat.EventContactData)
+	bot.Handle(`/contact`, func(evt webot.Event) {
+		data := evt.Data.(webot.EventContactData)
 		fmt.Println(`/contact` + data.GGID)
 	})
 
-	bot.Handle(`/login`, func(arg2 wechat.Event) {
+	bot.Handle(`/login`, func(arg2 webot.Event) {
 		isSuccess := arg2.Data.(int) == 1
 		if isSuccess {
 			fmt.Println(`login Success`)
@@ -40,8 +40,8 @@ func main() {
 
 	// 5s 发一次消息
 	bot.AddTimer(5 * time.Second)
-	bot.Handle(`/timer/5s`, func(arg2 wechat.Event) {
-		data := arg2.Data.(wechat.EventTimerData)
+	bot.Handle(`/timer/5s`, func(arg2 webot.Event) {
+		data := arg2.Data.(webot.EventTimerData)
 		if bot.IsLogin {
 			bot.SendTextMsg(fmt.Sprintf(`第%v次`, data.Count), `filehelper`)
 		}
@@ -49,8 +49,8 @@ func main() {
 
 	// 9:00 每天9点发一条消息
 	bot.AddTiming(`9:00`)
-	bot.Handle(`/timing/9:00`, func(arg2 wechat.Event) {
-		// data := arg2.Data.(wechat.EventTimingtData)
+	bot.Handle(`/timing/9:00`, func(arg2 webot.Event) {
+		// data := arg2.Data.(webot.EventTimingtData)
 		bot.SendTextMsg(`9:00 了`, `filehelper`)
 	})
 
